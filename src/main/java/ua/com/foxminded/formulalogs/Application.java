cee42715c92f07f31555a1eedfb007e451b39d70
Alexander Kropyvko <akropivko@gmail.com>
2020-12-07 14:36:37 +0200

    change absolute file path to relative

--- src/main/java/ua/com/foxminded/formulalogs/Application.java
+++ src/main/java/ua/com/foxminded/formulalogs/Application.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Application {
 
-    public static final String LOG_FILE_PATH = "c:/workspace/FormulaLogs/logfiles/";
+    public static final String LOG_FILE_PATH = "logfiles/";
     public static final String ABBREVIATIONS = "abbreviations.txt";
     public static final String START_LOG = "start.log";
     public static final String END_LOG = "end.log";
